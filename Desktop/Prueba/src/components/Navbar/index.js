5f47ac5be5e5cbb6d583f43ba4b8d4d880801975
Luis Mesa <luismesa818@gmail.com>
2021-06-25 03:15:01 +0200

    For deploy

---
+++ Desktop/Prueba/src/components/Navbar/index.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {FaBars} from 'react-icons/fa';
+import{Nav,
+NavbarContainer,
+NavLogo,
+MobileIcon,
+NavMenu,
+NavItem,
+NavLinks,
+NavBtn,
+NavBtnLink} from './NavbarElements';
+
+const Navbar = ({toggle}) => {
+    const {scrollNav} = useState(false)
+    
+    
+    
+    return (
+        <fragment>
+            <Nav scrollNav={scrollNav}>
+                <NavbarContainer>
+                    <NavLogo to='/'>EzZoom</NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='signup'>Sign Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+
+                </NavbarContainer>
+            </Nav>
+
+        </fragment>
+    );
+};
+
+export default Navbar
